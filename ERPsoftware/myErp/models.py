285695fa01aa5602e112f39d18801e481fc1a547
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-09 15:04:24 +0530

    added uomMaster

--- ERPsoftware/myErp/models.py
+++ ERPsoftware/myErp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class UOMmaster(models.Model):
+    unit = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.unit}"
