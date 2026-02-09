2bbb5dd27f47029a3f6eeb56043fafdf6f69eeeb
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-09 15:04:10 +0530

    added postgres database in settings.py

--- ERPsoftware/ERPsoftware/settings.py
+++ ERPsoftware/ERPsoftware/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ERPsoftware.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ERP',
+        'USER': 'postgres',
+        'PASSWORD': 'pratik1642',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
