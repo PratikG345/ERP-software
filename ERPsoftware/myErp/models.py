87df76cdc0ad126305e02ff8d94e37b0a7783447
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-10 15:19:15 +0530

    unit unitmaster table

--- ERPsoftware/myErp/models.py
+++ ERPsoftware/myErp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class UOMmaster(models.Model):
-    unit = models.CharField(max_length=100)
+    unit = models.CharField(max_length=10,default=None)
+    fullform = models.CharField(max_length=100,default=None)
     def __str__(self):
-        return f"{self.unit}"
+        return f"{self.unit}-{self.fullform}"
+    
