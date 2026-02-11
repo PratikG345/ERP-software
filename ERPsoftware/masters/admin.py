524146a35d7a905b37cde31f8c826872e2520614
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-11 16:09:44 +0530

    registered models in admin

--- ERPsoftware/masters/admin.py
+++ ERPsoftware/masters/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import UOMMaster,State,HSNCode,SACCode,Category,StockLocation,ItemMaster
 # Register your models here.
+admin.site.register(UOMMaster)
+admin.site.register(State)
+admin.site.register(HSNCode)
+admin.site.register(SACCode)
+admin.site.register(Category)
+admin.site.register(StockLocation)
+admin.site.register(ItemMaster)
