b656ada07bbd34b8a5700b1f998f8d5c3e232490
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-17 14:15:35 +0530

    created_at added to account master

--- ERPsoftware/masters/models.py
+++ ERPsoftware/masters/models.py
@@ -148,6 +148,7 @@ class AccountMaster(models.Model):
     AC_No = models.CharField(max_length=200,null=True,blank=True)
     IFSC_code = models.CharField(max_length=200,null=True,blank=True)
     Branch_code = models.CharField(max_length=200,null=True,blank=True)
+    created_at =  models.DateTimeField(auto_now_add=True,null=True)
     
     def __str__(self):
         return f"{self.ledger_name}"
