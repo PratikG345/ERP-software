77fa175dfc89560259a48df4eec698ea9c270766
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-17 15:09:21 +0530

    added urls for state

--- ERPsoftware/masters/urls.py
+++ ERPsoftware/masters/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import itemMaster,home,hsncode,saccode,stocklocation,category,uommaster,add_stck_loc,edit_stck_loc,delete_stck_loc
+from .views import itemMaster,home,hsncode,saccode,stocklocation,category,uommaster,add_stck_loc,edit_stck_loc,delete_stck_loc,state
 from .views import add_category,edit_category,delete_category
 from .views import add_unit,edit_unit,delete_unit
 from .views import add_hsn,edit_hsn,delete_hsn
 from .views import add_sac,edit_sac,delete_sac
 from .views import add_item,edit_item,delete_item
+from .views import add_state,edit_state,delete_state
 
 urlpatterns = [
     path('',home,name="home"),
@@ -13,6 +14,13 @@ urlpatterns = [
     path('itemMaster/add_item',add_item,name="add_item"),
     path('itemMaster/edit_item/<int:item_id>',edit_item,name="edit_item"),
     path('itemMaster/delete_item/<int:item_id>',delete_item,name="delete_item"),
+    #-------------------------------------------
+    
+    #-----------State----------------------
+    path('state/add_state',add_state,name="add_state"),
+    path('state/',state,name="state"),
+    path('state/edit_state/<int:state_id>',edit_state,name="edit_state"),
+    path('state/delete_state/<int:state_id>',delete_state,name="delete_state"),
     #-------------------------------------------
     
     #-----------HSN Code----------------------
