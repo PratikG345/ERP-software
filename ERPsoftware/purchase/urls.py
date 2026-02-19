f2295d66ed0fa6f3c8c8da7d36fcef131691c5cd
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-19 16:52:06 +0530

    added po type url

--- ERPsoftware/purchase/urls.py
+++ ERPsoftware/purchase/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path                                                
-from . import views
+from .views import po_type
+
+urlpatterns = [
+    path('po_type/',po_type,name='po_type'),
+]
