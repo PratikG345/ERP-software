de0e4bb25d9f7e1339c1986075ab69c64cdccc6f
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-19 16:51:06 +0530

    added purchase urls

--- ERPsoftware/ERPsoftware/urls.py
+++ ERPsoftware/ERPsoftware/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('masters.urls') ),
+    path('purchase/',include('purchase.urls'),)
 ]
