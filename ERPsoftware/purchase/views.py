a299ff8f1dc73fbcdf25f7bf5e657d78c827f83b
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-20 13:16:06 +0530

    added list view for po_type

--- ERPsoftware/purchase/views.py
+++ ERPsoftware/purchase/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import PO_type
 # Create your views
 
 def po_type(request):
-    return render(request,'purchase/po_type.html')
+    po = PO_type.objects.all()
+    return render(request,'purchase/po_type.html',{'po':po})
