e49bc5b9c0c5eec7ccf50721ae4fe1e391a4df5c
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-22 14:29:13 +0530

    added PurchaseOrder and PurchaseOrderLine in admin.py

--- ERPsoftware/purchase/admin.py
+++ ERPsoftware/purchase/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import PO_type
+from .models import PO_type,PurchaseOrder,PurchaseOrderLine
 # Register your models here.
 admin.site.register(PO_type)
+admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseOrderLine)
