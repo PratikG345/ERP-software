d9e42a99dff1a358346fd04f2261ef7cb32747fe
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-02-22 14:29:50 +0530

    created purchaseForm and purchaseLine form

--- ERPsoftware/purchase/forms.py
+++ ERPsoftware/purchase/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import PO_type
+from .models import PO_type,PurchaseOrder,PurchaseOrderLine
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -7,6 +7,16 @@ class PoTypeForm(forms.ModelForm):
     class Meta:
         model = PO_type
         fields = "__all__"
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = PurchaseOrder
+        fields = ['vendor','po_type','payment_terms','transport']
+        
+class PurchaseLine(forms.ModelForm):
+    
+    class Meta:
+        model = PurchaseOrderLine
+        fields = ['purchase_order','item','unit','qty','rate','igst_rate']
         
 class UserRegistrationForm(UserCreationForm):
     dept = forms.CharField(max_length=50)
